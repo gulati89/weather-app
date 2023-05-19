a3aba98b1163805cc1a2369fa7f124e93a7f52a7
gulati89 <jatin.gulati@agribazaar.com>
2023-05-19 23:17:36 +0530

    redirect issue resolve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes, {
       initialNavigation: 'enabledBlocking',
       preloadingStrategy: PreloadAllModules,
+      useHash: true
     }),
   ],
   exports: [RouterModule],
